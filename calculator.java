3bdb39418f9ed08643d0cdd331f1334fa64fbf0e
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-03-31 16:48:32 +0800

    大幅修改程式
    上1個程式的按鈕每一個名稱皆不同，在抓數字時要把名稱整個寫出來
    所以我把之前定義數字按鈕和四則按鈕刪掉，改為定義一個陣列，直接存取它的符號或數字
    另外在ActionListener方面，改成直接 implements
    actionPerformed 改成用 if-else 判斷式 來確認來源
    這次改成先用字串存取數字
    但是，第1個大數存取並沒完成，密碼設定還沒完成
    明天再朝這2個方向努力。

--- calculator.java
+++ calculator.java
@@ -1,106 +1,101 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-import javax.swing.border.TitledBorder;
-import java.util.Arrays;
-
-public class calculator extends JFrame{
-
-	int[] stringToArray = new int[];
-	String arrayToString = " ";
-	int inputIndex = 0;
-
-	private JButton jbtOne = new JButton("1");
-	private JButton jbtTwo = new JButton("2");
-	private JButton jbtThere = new JButton("3");
-	private JButton jbtFour = new JButton("4");
-	private JButton jbtFive = new JButton("5");
-	private JButton jbtSix = new JButton("6");
-	private JButton jbtSeven = new JButton("7");
-	private JButton jbtEight = new JButton("8");
-	private JButton jbtNine = new JButton("9");
-	private JButton jbtZero = new JButton("0");
-	private JButton jbtDelete = new JButton("CE");
-	private JButton jbtAllDelete = new JButton("ACE");
-	private JButton jbtAddition = new JButton("+");
-	private JButton jbtSubtraction = new JButton("-");
-	private JButton jbtMultiplication = new JButton("*");
-	private JButton jbtdivision = new JButton("/");
-	private JButton jbtEqual = new JButton("=");
-
-	private JTextField jtfDisplayZone = new JTextField();
-
-	public calculator() {
-
-		JPanel p1 = new JPanel(new GridLayout(4,3,5,5));
-
-		p1.add(jbtOne);
-		p1.add(jbtTwo);
-		p1.add(jbtThere);
-		p1.add(jbtFour);
-		p1.add(jbtFive);
-		p1.add(jbtSix);
-		p1.add(jbtSeven);
-		p1.add(jbtEight);
-		p1.add(jbtNine);
-		p1.add(jbtZero);
-		p1.add(jbtDelete);
-		p1.add(jbtAllDelete);
-
-		JPanel p2 = new JPanel(new GridLayout(3,2,5,5));
-
-		p2.add(jbtAddition);
-		p2.add(jbtSubtraction);
-		p2.add(jbtMultiplication);
-		p2.add(jbtdivision);
-		p2.add(jbtEqual);
-
-
-		add(jtfDisplayZone, BorderLayout.NORTH);
-		add(p1, BorderLayout.CENTER);
-		add(p2, BorderLayout.EAST);
-
-		jbtOne.addActionListener(new ButtonListener());
-		jbtTwo.addActionListener(new ButtonListener());
-		jbtThere.addActionListener(new ButtonListener());
-		jbtFour.addActionListener(new ButtonListener());
-		jbtFive.addActionListener(new ButtonListener());
-		jbtSix.addActionListener(new ButtonListener());
-		jbtSeven.addActionListener(new ButtonListener());
-		jbtEight.addActionListener(new ButtonListener());
-		jbtNine.addActionListener(new ButtonListener());
-		jbtZero.addActionListener(new ButtonListener());
-		jbtDelete.addActionListener(new ButtonListener());
-		jbtAllDelete.addActionListener(new ButtonListener());
-		jbtAddition.addActionListener(new ButtonListener());
-		jbtSubtraction.addActionListener(new ButtonListener());
-		jbtMultiplication.addActionListener(new ButtonListener());
-		jbtdivision.addActionListener(new ButtonListener());
-		jbtEqual.addActionListener(new ButtonListener());
-	}
-
-		private class ButtonListener implements ActionListener {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				// Get values from text field.
-				if (e.getSource() == jbtOne) {
-					stringToArray[inputIndex] = 1;
-					inputIndex = inputIndex + 1;
-				} else {
-					stringToArray[inputIndex] = 0;
-					inputIndex = inputIndex + 1;
-				}
-				arrayToString = Arrays.toString(stringToArray);
-				jtfDisplayZone.setText(arrayToString);
-			}
-		}
-		public static void main(String[] args) {
-			/** Main method */
-			calculator frame = new calculator(); // Create a frame.
-			frame.setSize(350,300); // Set the frame size.
-			frame.setTitle("Calculator");
-			frame.setLocationRelativeTo(null);
-			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-			frame.setVisible(true);
-		}
+ 
+public class calculator extends JFrame implements ActionListener{
+        
+    JTextField numberfield;
+    
+    String numberstring="";
+    
+    double number;
+    
+    char buttonname[]={'7','8','9','/',
+                       '4','5','6','*',
+                       '1','2','3','-',
+                       '0','.','=','+'};
+                       
+    JButton button[]=new JButton[16];
+    
+    char operator;
+   
+    boolean point;
+   
+    public calculator()
+    {        
+        super("123");
+ 
+        Container c=getContentPane();
+        c.setLayout(new BorderLayout());
+ 
+        JPanel p=new JPanel();
+ 
+        numberfield=new JTextField("0.0");
+        numberfield.setHorizontalAlignment(JTextField.RIGHT);
+ 
+        p.setLayout(new GridLayout(4,4));
+      
+        for(int i=0;i<16;i++){
+            button[i]=new JButton(buttonname[i]+"");
+            p.add(button[i]);
+            button[i].addActionListener(this);
+        }
+ 
+        c.add("North",numberfield);
+        c.add("Center",p);
+      
+        setSize(200,200);
+        setVisible(true);
+    }
+ 
+    public void actionPerformed(ActionEvent event)
+    {   
+        if(((JButton)event.getSource()).getText().charAt(0)=='.'){
+            if(!point){
+                if(numberstring.equals(""))
+                    numberstring="0";
+                    
+                numberstring+=".";
+                numberfield.setText(numberstring);
+                point=true; 
+           }
+        }else if(((JButton)event.getSource()).getText().charAt(0)<='9'&&
+                 ((JButton)event.getSource()).getText().charAt(0)>='0'){
+            numberstring+=((JButton)event.getSource()).getText().charAt(0)+"";
+            numberfield.setText(numberstring);
+        }else{
+            point=false;
+            numberstring="";
+            numberfield.setText(operation(number,
+                    Double.parseDouble(numberfield.getText()),operator)+"");
+            operator=((JButton)event.getSource()).getActionCommand().charAt(0);
+            
+            if(((JButton)event.getSource()).getText().charAt(0)!='=')                  
+                number=Double.parseDouble(numberfield.getText());               
+        }         
+    }
+   
+    public double operation(double number1,double number2,char operator)
+    {      
+        switch(operator)
+        {        
+            case '+':
+                return number1+number2;
+            case '-':
+                return number1-number2;
+            case '*':
+                return number1*number2;
+            case '/':
+                return number1/number2;
+            default:
+                return number2; 
+        }
+    }
+       
+    public static void main(String args[])
+    {      
+        cal a=new cal();
+        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
+    }
+ 
 }
