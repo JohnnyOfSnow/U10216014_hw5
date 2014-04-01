58e700f5ad0555be8828af5b5dfb79ce804f9306
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-04-01 11:06:53 +0800

    TestPassword 除錯完成
    除錯過程:
    原本字元陣列，後來我用String.valueOf(passwordField.getPassword()); 把字元陣列轉成字串
    然後 password == userPassword 不管有沒有等於都不對 因為String 是物件 不能用 = 來比較
    於是我改成 password.equals(userPassword)

--- TestPassword.java
+++ TestPassword.java
@@ -8,10 +8,10 @@ public class TestPassword extends JFrame {
 	TestPassword() {
 		JPanel p1 = new JPanel(new GridLayout(2,1));
 		passwordField = new JPasswordField();
-		passwordField.setBounds(175, 120, 100, 20);
 		passwordField.setEchoChar('*');
 		p1.add(passwordField);
 		p1.add(jbtEnter);
+		add(p1, BorderLayout.CENTER);
 		// set the button event that user click it.
 		jbtEnter.addActionListener(new ButtonListener());
 	}
@@ -20,10 +20,17 @@ public class TestPassword extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			String password = "0000";
-			if(passwordField.getPassword() == password){
+			String userPassword = "";
+			userPassword = String.valueOf(passwordField.getPassword());
+			if(password.equals(userPassword)){
+				System.out.println(userPassword);
+				System.out.println(password);
 				System.out.println("You can use calculator");
 			} else {
 				System.out.println("You can't use calculator");
+				System.out.println(userPassword);
+				System.out.println(password);
+				userPassword = " ";
 			}
 			
 		}
@@ -33,7 +40,7 @@ public class TestPassword extends JFrame {
 		/** Main method */
 		TestPassword frame = new TestPassword(); // Create a frame.
 		frame.setSize(350,300); // Set the frame size.
-		frame.setTitle("BMI Calculator");
+		frame.setTitle("Calculator");
 		frame.setLocationRelativeTo(null);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.setVisible(true);
