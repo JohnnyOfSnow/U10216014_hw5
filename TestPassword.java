04ebbc2bdcc1298fa718d9f973e6779667806223
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-04-01 10:49:45 +0800

    增加密碼框和按鈕，以及除錯中
    增加密碼框與按鈕
    動作為:按下按鈕後抓取密碼框的東西進到判斷式比對
    但是抓進來的是字元陣列，所以我用字串比不對。

--- TestPassword.java
+++ TestPassword.java
@@ -1,16 +1,41 @@
-import java.awt.FlowLayout;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JFrame;
-import javax.swing.JTextField;
-import javax.swing.JPasswordField;
-import javax.swing.JOptionPane;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
 
-public class TestPassword {
+public class TestPassword extends JFrame {
+	private JPasswordField passwordField;
+	private JButton jbtEnter = new JButton("Enter");
 	TestPassword() {
-
+		JPanel p1 = new JPanel(new GridLayout(2,1));
+		passwordField = new JPasswordField();
+		passwordField.setBounds(175, 120, 100, 20);
+		passwordField.setEchoChar('*');
+		p1.add(passwordField);
+		p1.add(jbtEnter);
+		// set the button event that user click it.
+		jbtEnter.addActionListener(new ButtonListener());
 	}
+
+	private class ButtonListener implements ActionListener {
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			String password = "0000";
+			if(passwordField.getPassword() == password){
+				System.out.println("You can use calculator");
+			} else {
+				System.out.println("You can't use calculator");
+			}
+			
+		}
+	}
+
 	public static void main(String[] args) {
-		
+		/** Main method */
+		TestPassword frame = new TestPassword(); // Create a frame.
+		frame.setSize(350,300); // Set the frame size.
+		frame.setTitle("BMI Calculator");
+		frame.setLocationRelativeTo(null);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
 	}
 }
