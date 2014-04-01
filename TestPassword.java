93cf298145ccd737686569ba4e9ff4bdd19117f0
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-04-01 10:16:36 +0800

    Create TestPassword.java
    這個程式先試做密碼輸入的這塊模組

---
+++ TestPassword.java
@@ -0,0 +1,16 @@
+import java.awt.FlowLayout;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import javax.swing.JOptionPane;
+
+public class TestPassword {
+	TestPassword() {
+
+	}
+	public static void main(String[] args) {
+		
+	}
+}
