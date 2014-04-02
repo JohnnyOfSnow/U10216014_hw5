bd2f3d15b3382a602377c63ad8cd60e1f46cd035
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-04-02 10:30:23 +0800

    創建了ComputePasswordDialog.java
    這個程式是為了加分題第2題的試做
    要讓使用者能創密碼(題目)
    思考:一定要有檔案之類的東西讀才有可能做到
    於是我先檢查檔案存不存在，如果不存在的話，就要要求使用者輸入密碼(然後存成文字檔)
    以便下一次使用時能找到密碼(這部分我覺得不用用到Counter 因為檔案存在就表示設定過密碼，所以不需要設這個變數)
    其次，當檔案存在時，需要做讀檔的動作(這裡我使用了 Scanner)，除錯方面，我在讀檔那寫了一行印出(檢查密碼是否正確)

---
+++ ComputePasswordDialog.java
@@ -0,0 +1,25 @@
+import javax.swing.JOptionPane; // textbook p.97
+import java.util.Scanner;
+
+public class ComputePasswordDialog {
+	public static void main(String[] args) throws Exception{
+		
+		String alreadyPassword = " ";
+		java.io.File file = new java.io.File("password.txt");
+		if(file.exists()) {
+			Scanner input = new Scanner(file);
+			while (input.hasNext()) {
+				alreadyPassword = input.next(); // Read items
+				System.out.println(alreadyPassword); // Debug print
+			}
+			input.close(); // Close the file.
+
+		} else {
+			String userPassword = JOptionPane.showInputDialog("Enter the password that you want, for example, 4758");
+			java.io.PrintWriter output = new java.io.PrintWriter(file);
+			output.print(userPassword);
+
+			output.close();
+		}
+	}
+}
