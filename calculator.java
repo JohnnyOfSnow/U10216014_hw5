75bc09d0129d75c3d86983d93be8b94d3b34147a
ProgramExercise征戰 <good258johnny@yahoo.com.tw>
2014-04-02 14:15:38 +0800

    作業完成
    這次修改的地方:
    1.加入讀檔寫檔的程式碼(為第1次開啟程式的使用者需先設定密碼，之後開啟時會根據password.txt 裡的密碼作為開啟計算機的密碼)
    2.(1.的補充)當在第1次設定密碼時若不設定將會直接關閉程式
    3.在程式碼前加入註解
    4.針對該註解的程式碼附上註解

--- calculator.java
+++ calculator.java
@@ -1,12 +1,23 @@
+/**
+ * Name: Johnny
+ * ID: U10216014
+ * Ex: hw5(write a calculator)
+ * Information: 
+ *      The program simulate a calculator.
+ *      If user frist time use the calculator, he/she must set a password in order to use next time.
+ *      If the password had setted, the program request user enter password.
+ *      The calculator have addition, Subtraction, multiplication, and division.
+ */
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
- 
+import java.util.*; 
+
 public class calculator extends JFrame implements ActionListener{
         
     JTextField numberfield;
     
-    String numberstring="";
+    String numberstring = " ";
     
     double number;
     
@@ -22,15 +33,15 @@ public class calculator extends JFrame implements ActionListener{
     boolean point;
    
     public calculator()
-    {        
-        super("123");
+    {
+        super("30元計算機");
  
-        Container c=getContentPane();
+        Container c = getContentPane();
         c.setLayout(new BorderLayout());
  
-        JPanel p=new JPanel();
+        JPanel p = new JPanel();
  
-        numberfield=new JTextField("0.0");
+        numberfield = new JTextField("0.0");
         numberfield.setHorizontalAlignment(JTextField.RIGHT);
  
         p.setLayout(new GridLayout(4,4));
@@ -41,15 +52,14 @@ public class calculator extends JFrame implements ActionListener{
             button[i].addActionListener(this);
         }
  
-        c.add("North",numberfield);
-        c.add("Center",p);
+        c.add("North",numberfield); // The textfield in the north of frame.
+        c.add("Center",p); // The JPanel(p) in the north of frame.
       
         setSize(200,200);
         setVisible(true);
     }
  
-    public void actionPerformed(ActionEvent event)
-    {   
+    public void actionPerformed(ActionEvent event){
         if(((JButton)event.getSource()).getText().charAt(0)=='.'){
             if(!point){
                 if(numberstring.equals(""))
@@ -57,7 +67,7 @@ public class calculator extends JFrame implements ActionListener{
                     
                 numberstring+=".";
                 numberfield.setText(numberstring);
-                point=true; 
+                point=true;
            }
         }else if(((JButton)event.getSource()).getText().charAt(0)<='9'&&
                  ((JButton)event.getSource()).getText().charAt(0)>='0'){
@@ -70,15 +80,14 @@ public class calculator extends JFrame implements ActionListener{
                     Double.parseDouble(numberfield.getText()),operator)+"");
             operator=((JButton)event.getSource()).getActionCommand().charAt(0);
             
-            if(((JButton)event.getSource()).getText().charAt(0)!='=')                  
-                number=Double.parseDouble(numberfield.getText());               
-        }         
+            if(((JButton)event.getSource()).getText().charAt(0)!='=')
+                number=Double.parseDouble(numberfield.getText());
+        }
     }
    
-    public double operation(double number1,double number2,char operator)
-    {      
+    public double operation(double number1,double number2,char operator){
         switch(operator)
-        {        
+        {
             case '+':
                 return number1+number2;
             case '-':
@@ -88,33 +97,60 @@ public class calculator extends JFrame implements ActionListener{
             case '/':
                 return number1/number2;
             default:
-                return number2; 
+                return number2;
         }
     }
        
-    public static void main(String args[])
-    {   
-
+    public static void main(String args[]) throws Exception {
         /** Main method */
-        TestPassword frame1 = new TestPassword(); // Create a frame.
-        frame1.setSize(350,300); // Set the frame size.
-        frame1.setTitle("Calculator");
-        frame1.setLocationRelativeTo(null);
-        frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame1.setVisible(true);
+        String alreadyPassword = " ";
+        java.io.File file = new java.io.File("password.txt");
 
-              
+        /**
+         *   password.txt exist =====> read password
+         *   password.txt doesn't exist======> let user input password to use next tine.
+         */
+        if(file.exists()) {
+            Scanner input = new Scanner(file);
+            while (input.hasNext()) {
+                alreadyPassword = input.next(); // Read items
+            }
+            input.close(); // Close the file.
+            TestPassword frame1 = new TestPassword(); // Create a frame.
+            frame1.setPassword(alreadyPassword);
+            frame1.setSize(350,300); // Set the frame size.
+            frame1.setTitle("Calculator");
+            frame1.setLocationRelativeTo(null);
+            frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            frame1.setVisible(true);  
+
+        } else {
+            String userPassword = JOptionPane.showInputDialog("第1次使用計算機需先輸入你想設定的密碼");
+            if(userPassword == null) {
+                        System.exit(0); // Exit the program.
+            } else {
+                        
+                        java.io.PrintWriter output = new java.io.PrintWriter("password.txt");
+                        output.print(userPassword); // Write password to the password.txt
+                        output.close();
+                        calculator a = new calculator(); // Open the calculator.
+                        a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
+            }                                                  
+        }
+          
     }
- 
 }
 
 class TestPassword extends JFrame {
-    // Create JButton  JTextField ,and JPasswordField.
-        private JButton jbtEnter = new JButton("Enter");
-    private JTextField jtfDisplayResult = new JTextField("輸入你的密碼在上方的空格內");
+    // Create JButton JTextField ,and JPasswordField.
+    private JButton jbtEnter = new JButton("Enter");
+    private JPasswordField passwordField;
+    private JTextField jtfDisplayResult = new JTextField("在上方的空格中輸入計算機密碼");
+    String password = "0000"; 
 
     TestPassword() {
-        // Create a panel to hold JButton  JTextField ,and JPasswordField.
+
+        // Create a panel to hold JButton JTextField ,and JPasswordField.
         JPanel p1 = new JPanel(new GridLayout(3,1));
 
         // Set the passwordField attributes.
@@ -134,24 +170,30 @@ class TestPassword extends JFrame {
         jbtEnter.addActionListener(new ButtonListener()); // Register listener.
     }
 
+    // A set method to set password
+    public void setPassword(String newPassword) {
+        password = newPassword;
+    }
+
+    // A get method to get password
+    public String getPassword() {
+        return password;
+    }
+
     private class ButtonListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            String password = "0000"; 
+            password = getPassword();
             String userPassword = " "; // User key in the passwordField.
-
             userPassword = String.valueOf(passwordField.getPassword());
             if(password.equals(userPassword)){
-                dispose(); // close the window
-                calculatorTwo a = new calculatorTwo();
-                a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);  
-                 
+                dispose(); // close the window 
+                calculator a = new calculator(); // Open the calculator.
+                a.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);   
             } else {
-                jtfDisplayResult.setText("你現在不可以使用計算機，但我再給你機會再輸入一次密碼");
+                jtfDisplayResult.setText("你的密碼是錯的，我再給你一次機會輸入密碼");
                 userPassword = " "; // Initialization.
-            }
-            
+            }         
         }
     }
-
-}private JPasswordField passwordField;
+}
